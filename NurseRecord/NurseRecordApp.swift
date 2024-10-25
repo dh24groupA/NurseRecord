0feab22d1bd949b36f26c9a2a2bfd07abd5feacf
haruki adachi <adaharu0501@gmail.com>
2024-10-25 17:25:14 +0900

    Initial Commit

---
+++ NurseRecord/NurseRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  NurseRecordApp.swift
+//  NurseRecord
+//
+//  Created by デジタルヘルス on 2024/10/25.
+//
+
+import SwiftUI
+
+@main
+struct NurseRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
